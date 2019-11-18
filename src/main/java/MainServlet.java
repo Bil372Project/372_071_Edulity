7b4787c558d935d233e6eb9df517f1000f0bf50a
Muhammed Emre Durdu <muhammedemredurdu@gmail.com>
2019-11-18 03:07:59 +0300

    schoolpage initial design

--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -5,9 +5,7 @@ import org.hibernate.query.Query;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.*;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.util.Hashtable;
 import java.util.List;
@@ -15,16 +13,30 @@ import java.util.List;
 @WebServlet(urlPatterns = {"/mainServlet"})
 public class MainServlet extends HttpServlet {
 
-    List<SchoolEntity> schoolEntities = null;
 
     @Override
+
     public void init() throws ServletException {
         super.init();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+
         Hashtable errors = validate(request);
+
+        //if invoked the go to school page submit button
+        if(request.getParameter("submit").equals("Go to school page")){
+            session.setAttribute("school_name", request.getParameter("school_name"));
+            request.getRequestDispatcher("/school.jsp").forward(request,response);
+            return;
+        }
+
         if(errors.isEmpty()) { // validation succesfull
+            session.setAttribute("id", request.getParameter("id"));
+            session.setAttribute("type", request.getParameter("type"));
+            session.setAttribute("school_name", request.getParameter("school_name"));
             request.getRequestDispatcher("/student.jsp").forward(request,response);
         }
         else {
@@ -35,7 +47,7 @@ public class MainServlet extends HttpServlet {
 
     private Hashtable validate(HttpServletRequest request) {
         Hashtable errors = new Hashtable();
-        String schoolName = request.getParameter("school_names");
+        String schoolName = request.getParameter("school_name");
         String type = request.getParameter("type");
         String id = request.getParameter("id");
         if(schoolName.equals("")) {
